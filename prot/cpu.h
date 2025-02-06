9e47ad24d96a0b3f731f02b0c85acb9160c2011e
miwate <williamduo53@gmail.com>
2025-02-06 15:33:44 +0100

    tuto git pour enzo, + proto

--- prot/cpu.h
+++ prot/cpu.h
@@ -12,55 +12,27 @@ typedef struct processeur
     int PC;
     int SP;
     int memoire[5000];
-    
+
 } Processeur;
 
 /* -- Protoytpes de fonctions -- */
 
 /* Met le contenu du haut de la pile à l’adresse x et décrémente SP*/
-void pop(Processeur *cpu, int x)
-{
-    cpu->SP--;
-    cpu->memoire[x] = cpu->memoire[cpu->SP];
-}
+void pop(Processeur *cpu, int x);
 
 /* Met le contenu du haut de la pile -1 à l’adresse n, où n est la valeur du sommet de la pile décrémente SP de deux*/
-void ipop(Processeur *cpu)
-{
-    cpu->SP -= 2;
-    int n = cpu->memoire[cpu->SP];
-    cpu->memoire[n] = cpu->memoire[cpu->SP - 1];
-}
+void ipop(Processeur *cpu);
 
-/* empile le contenu de l’adresse x (et donc incrémente ensuite SP) */
-void push(Processeur *cpu, int x)
-{
-    cpu->memoire[cpu->SP] = cpu->memoire[x];
-    cpu->SP++;
-}
+/* empile le contenu de l’adresse x (et donc incrémente ensuite SP); */
+void push(Processeur *cpu, int x);
 
-/* empile la valeur i (et donc incrémente ensuite SP) */
-void ipush(Processeur *cpu, int i)
-{
-    cpu->memoire[cpu->SP] = i;
-    cpu->SP++;
-}
+/* empile la valeur i (et donc incrémente ensuite SP); */
+void ipush(Processeur *cpu, int i);
 
-/* additionne adr au registre PC (adr peut être négatif) */
-void jmp(Processeur *cpu, int adr)
-{
-    cpu->PC += adr;
-}
+/* additionne adr au registre PC (adr peut être négatif); */
+void jmp(Processeur *cpu, int adr);
 
 /* dépile un élément. Si celui-ci n’est pas nul, additionne adr au registre PC */
-void jnz(Processeur *cpu, int adr)
-{
-    cpu->SP--;
-    if (cpu->memoire[cpu->SP] != 0)
-    {
-        cpu->PC += adr;
-    }
-}
-
+void jnz(Processeur *cpu, int adr);
 
 #endif
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Petit Tuto Git
+
+## Commandes essentielles
+
+1. **Vérifier l'état du dépôt**
+   git status
+
+2. **Ajouter tous les fichiers modifiés**
+   git add .
+
+3. **Encore vérifier l'état du dépôt**
+   git status
+
+4. **Valider les changements avec un message**
+   git commit -a -m "Message détaillé"
+
+5. ** Encore encore vérifier l'état du dépôt**
+   git status
+
+6. **Envoyer les modifications vers le dépôt distant**
+   git push
+
